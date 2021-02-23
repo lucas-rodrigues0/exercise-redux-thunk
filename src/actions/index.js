43db4a475a4933424a724be70c4501a7f9b29475
Lucas Rodrigues <lucas.movimento@gmail.com>
2021-02-23 19:18:51 -0300

    realiza exercise-one

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,4 +7,10 @@ export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
   // Desenvolva aqui o código da action assíncrona
+  return (dispatch) => {
+    dispatch(requestAPI());
+    return fetch('https://api.thecatapi.com/v1/images/search')
+      .then((response) => response.json())
+      .then((data) => dispatch(getPicture(data[0])))
+  }
 }
